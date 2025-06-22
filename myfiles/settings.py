9996f7778cc8092e9fc5c6f07bc45887b5184896
Vikash kumar <yyvikash63@gmail.com>
2025-06-22 13:06:58 +0530

    Update settings.py

--- myfiles/settings.py
+++ myfiles/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "fallback-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['*']
